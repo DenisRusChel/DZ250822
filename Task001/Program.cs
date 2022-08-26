6a4833cdacae6e8dfa31e5297d07b054aa31d0ae
«Denis» <tytdenis2@gmail.com>
2022-08-26 16:18:05 +0500

    add Task001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.WriteLine("Введите 5-ти значное число: ");
+string number = Console.ReadLine();
+
+if (number.Length == 5)
+{
+    Palindrome(number);
+}
+else
+{
+    System.Console.WriteLine("Введите верное число!");
+}
+void Palindrome(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    System.Console.WriteLine($"{number} → да");
+    else
+    System.Console.WriteLine($"{number} → нет");
+}
